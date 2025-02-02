797fef809e59de190a3c9f1f01d1f6553cb488fb
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-02-02 21:18:19 +0200

    Updated server port

--- server/server.js
+++ server/server.js
@@ -1399,7 +1399,11 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    // const port = 3030; 
+    let port = process.argv[2];
+    if (!port) port = process.env['PORT']; 
+    if (!port) port = 8080;
+
     server.listen(port);
     console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
     console.log(`Admin panel located at http://localhost:${port}/admin`);
